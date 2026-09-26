2b264120b083466d783e03f726c92aef6aa90779
whitedevil-070706 <Dakshanushabindupankaj@gmail.com>
2026-09-26 21:49:17 +0530

    update

--- factor_of_any_number.java
+++ factor_of_any_number.java
@@ -1,4 +1,18 @@
 package daksh;
 
+import java.util.Scanner;
+
 public class factor_of_any_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+        System.out.println("These are the factors of " + n + "are :" );
+        for (int i = 1; i <= n; i++) {
+            if(n % i ==0){
+                System.out.println( i + " ");
+            }
+        }
+    }
 }
+l
